a5e845557fe321cf28ac888323aaba3b66103708
jeminkhunt75 <jeminkhun7@gmail.com>
2024-05-24 22:13:42 +0530

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,45 @@
-$(document).ready(function() {
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+      elmnt = z[i];
+      /*search for elements with a certain atrribute:*/
+      file = elmnt.getAttribute("include-html");
+      if (file) {
+          /* Make an HTTP request using the attribute value as the file name: */
+          xhttp = new XMLHttpRequest();
+          xhttp.onreadystatechange = function () {
+              if (this.readyState == 4) {
+                  if (this.status == 200) {
+                      elmnt.innerHTML = this.responseText;
+                  }
+                  if (this.status == 404) {
+                      elmnt.innerHTML = "Page not found.";
+                  }
+                  /* Remove the attribute, and call this function once more: */
+                  elmnt.removeAttribute("include-html");
+                  includeHTML();
+              }
+          };
+          xhttp.open("GET", file, true);
+          xhttp.send();
+          /* Exit the function: */
+          return;
+      }
+  }
+}
+includeHTML();
+
   var navToggle = $(".toggle-menu");
   var navWrapper = $(".nav__wrapper");
 
-  navToggle.on("click", function() {
-    if (navWrapper.hasClass("active")) {
+  $(document).on("click", ".toggle-menu", function() {
+    if ($(".nav__wrapper").hasClass("active")) {
       $(this).attr("aria-expanded", "false").attr("aria-label", "menu").removeClass("active");
-      navWrapper.removeClass("active");
+      $(".nav__wrapper").removeClass("active");
     } else {
-      navWrapper.addClass("active");
+      $(".nav__wrapper").addClass("active");
       $(this).attr("aria-label", "close menu").attr("aria-expanded", "true").addClass("active");
     }
   });
@@ -96,8 +128,6 @@ $(document).ready(function() {
     });
    });
    
-});
-
 
   /**
    * Init swiper sliders
